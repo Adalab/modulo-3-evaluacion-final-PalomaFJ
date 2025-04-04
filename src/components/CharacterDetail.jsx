13aa35ad3c802662eadb6788e73c01582fec8d00
Paloma Fernandez Jaen <palomafj91@gmail.com>
2025-04-04 16:14:41 +0200

    iconos del estado del personaje

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,30 +1,37 @@
 import { useParams } from "react-router-dom";
 
 function CharacterDetail({ characters }) {
-  //uso el useParams pra recoger el id de la url 
-  const { id } = useParams();
-  
-  // busco el persnaje que tenga ese id
-  // convierto su id a numero xq viene como un string y me devuelve false 
-  const character = characters.find(
-    (char) => String(char.id) === id //asi los dosson texto y el find puede funcionar bien a la hora de comparar
-  );
+    //uso el useParams pra recoger el id de la url 
+    const { id } = useParams();
 
-  // Si no se encuentra el personaje muestro un mensaje
-  if (!character) {
-    return <p>Personaje no encontrado</p>;
-  }
+    // busco el persnaje que tenga ese id
+    // convierto su id a numero xq viene como un string y me devuelve false 
+    const character = characters.find(
+        (char) => String(char.id) === id //asi los dosson texto y el find puede funcionar bien a la hora de comparar
+    );
 
-  // Si si se encuenra el personaje lo muestro
-  return (
-    <section className="character-detail">
-      <h2 className="detail-name">{character.name}</h2>
-      <img className="detail-img" src={character.image} alt={character.name} />
-      <p className="detail-info">Especie: {character.species}</p>
-      <p className="detail-info">Estado: {character.status}</p>
-      <p className="detail-info">Origen: {character.origin.name}</p>
-    </section>
-  );
+    // Si no se encuentra el personaje muestro un mensaje
+    if (!character) {
+        return <p>Personaje no encontrado</p>;
+    }
+
+    // Si si se encuenra el personaje lo muestro
+    return (
+        <section className="character-detail">
+            <h2 className="detail-name">{character.name}</h2>
+            <img className="detail-img" src={character.image} alt={character.name} />
+            <p className="detail-info">Especie: {character.species}</p>
+            <p className="detail-info">
+                Estado:{" "}
+                {character.status === "Alive"
+                    ? "🟢 Vivo"
+                    : character.status === "Dead"
+                        ? "🔴 Muerto"
+                        : "⚪ Desconocido"}
+            </p>
+            <p className="detail-info">Origen: {character.origin.name}</p>
+        </section>
+    );
 }
 
 export default CharacterDetail;
